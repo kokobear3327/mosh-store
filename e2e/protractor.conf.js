f46ba5ee53295de6bfea5f567eebe4b145f1707a
Richard Pitts <richard.pitts@umbragestudios.com>
2020-01-15 15:51:34 -0600

    feature: prettier installed, config file setup for single quotes, and ran in terminal

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,22 +1,22 @@
-const { SpecReporter } = require("jasmine-spec-reporter");
+const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: ["./src/**/*.e2e-spec.ts"],
+  specs: ['./src/**/*.e2e-spec.ts'],
   capabilities: {
-    browserName: "chrome"
+    browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: "http://localhost:4200/",
-  framework: "jasmine",
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
   onPrepare() {
-    require("ts-node").register({
-      project: require("path").join(__dirname, "./tsconfig.json")
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.json')
     });
     jasmine
       .getEnv()
